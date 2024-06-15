447db7289eb76133e24429f9c039d5b751b74eb2
Electronic Mango <78230210+Electronic-Mango@users.noreply.github.com>
2024-06-15 14:32:59 +0200

    Minor tweaks

--- main.py
+++ main.py
@@ -19,15 +19,14 @@ def user(subreddit: str, request: Request):
     params = dict(request.query_params)
     params |= {"action": "display", "bridge": "RedditBridge", "context": "user", "format": "Mrss"}
     rss_bridge_response = get(RSS_BRIDGE_URL, params=params)
-    filtered_response = filter(rss_bridge_response.text.encode(ENCODING), subreddit)
+    filtered_response = filter_rss(rss_bridge_response.text, subreddit)
     return PlainTextResponse(filtered_response)
 
 
-def filter(xml: str, subreddit: str) -> str:
-    tree = fromstring(xml)
+def filter_rss(xml: str, subreddit: str) -> str:
+    tree = fromstring(xml.encode(ENCODING))
     subreddit_filter = f"{SUBREDDIT_FILTER.format(subreddit=subreddit)}"
     items = tree.xpath(f"//item[not(starts-with(link,'{subreddit_filter}'))]")
     for item in items:
         item.getparent().remove(item)
     return tostring(tree, xml_declaration=True, pretty_print=True, encoding=ENCODING)
-
